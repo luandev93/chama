4f4779ebb881121374f78b80f2651f59982c6d1c
luandev93 <luandev93@gmail.com>
2026-08-25 15:28:47 -0300

    feat(tenancy): enforce tenant context outside health checks

---
+++ src/core/tenancy/tenancy.module.ts
@@ -0,0 +1,9 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { TenantContextMiddleware } from './tenant-context.middleware';
+
+@Module({})
+export class TenancyModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TenantContextMiddleware).exclude({ path: 'api/health', method: RequestMethod.GET }).forRoutes('*');
+  }
+}

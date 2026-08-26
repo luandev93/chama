ab7a30da8295edada1dd64086c402299e9493052
luandev93 <luandev93@gmail.com>
2026-08-26 13:23:52 -0300

    feat(tenancy): enforce store access globally

--- src/core/tenancy/tenancy.module.ts
+++ src/core/tenancy/tenancy.module.ts
@@ -1,7 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TenantContextMiddleware } from './tenant-context.middleware';
+import { StoreAccessGuard } from './store-access.guard';
 
-@Module({})
+@Module({
+  providers: [{ provide: APP_GUARD, useClass: StoreAccessGuard }],
+})
 export class TenancyModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(TenantContextMiddleware).exclude(

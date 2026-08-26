c52ec26dcbdf32a26dfc52dc783f387db68a5016
luandev93 <luandev93@gmail.com>
2026-08-26 14:06:23 -0300

    feat(inventory): register reservation service

--- src/modules/inventory/inventory.module.ts
+++ src/modules/inventory/inventory.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { StockReservationService } from './stock-reservation.service';
 
 @Module({
   controllers: [InventoryController],
-  providers: [InventoryService],
+  providers: [InventoryService, StockReservationService],
+  exports: [InventoryService, StockReservationService],
 })
 export class InventoryModule {}

bed946514a2920cd90f6c8d062346c17c4e4c62f
luandev93 <luandev93@gmail.com>
2026-08-26 14:04:35 -0300

    feat(pricing): add validated price preview dtos

---
+++ src/modules/catalog/dto/pricing-preview.dto.ts
@@ -0,0 +1,22 @@
+import { Transform } from 'class-transformer';
+import { IsDecimal, IsString } from 'class-validator';
+
+export class PriceFromMarkupDto {
+  @Transform(({ value }) => String(value))
+  @IsDecimal({ decimal_digits: '0,4', force_decimal: false })
+  costPrice!: string;
+
+  @Transform(({ value }) => String(value))
+  @IsDecimal({ decimal_digits: '0,4', force_decimal: false })
+  markupPercent!: string;
+}
+
+export class MarginFromPriceDto {
+  @Transform(({ value }) => String(value))
+  @IsDecimal({ decimal_digits: '0,4', force_decimal: false })
+  costPrice!: string;
+
+  @Transform(({ value }) => String(value))
+  @IsDecimal({ decimal_digits: '0,4', force_decimal: false })
+  salePrice!: string;
+}

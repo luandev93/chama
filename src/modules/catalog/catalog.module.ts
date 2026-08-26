bdee2858e1537d179aac9ba3d88b330a2fe992a6
luandev93 <luandev93@gmail.com>
2026-08-26 14:04:50 -0300

    feat(catalog): wire category section and pricing policy services

--- src/modules/catalog/catalog.module.ts
+++ src/modules/catalog/catalog.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CatalogController } from './catalog.controller';
+import { CatalogTaxonomyController } from './catalog-taxonomy.controller';
 import { CatalogService } from './catalog.service';
+import { CatalogTaxonomyService } from './catalog-taxonomy.service';
 import { PricingService } from './pricing.service';
 
 @Module({
-  controllers: [CatalogController],
-  providers: [CatalogService, PricingService],
+  controllers: [CatalogController, CatalogTaxonomyController],
+  providers: [CatalogService, CatalogTaxonomyService, PricingService],
   exports: [PricingService],
 })
 export class CatalogModule {}

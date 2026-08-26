91f67e65c6a251a6fccdb6e587697c5aca60dcc2
luandev93 <luandev93@gmail.com>
2026-08-26 14:03:58 -0300

    feat(pricing): register pricing engine

--- src/modules/catalog/catalog.module.ts
+++ src/modules/catalog/catalog.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatalogController } from './catalog.controller';
 import { CatalogService } from './catalog.service';
+import { PricingService } from './pricing.service';
 
 @Module({
   controllers: [CatalogController],
-  providers: [CatalogService],
+  providers: [CatalogService, PricingService],
+  exports: [PricingService],
 })
 export class CatalogModule {}

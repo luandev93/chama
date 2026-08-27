79a290d0cd587c013c9dc6da2922ecb2202a909a
luandev93 <luandev93@gmail.com>
2026-08-27 05:35:59 -0300

    feat(payments): add secure payment domain module

---
+++ src/modules/payments/payments.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+
+@Module({ providers: [PaymentsService], exports: [PaymentsService] })
+export class PaymentsModule {}

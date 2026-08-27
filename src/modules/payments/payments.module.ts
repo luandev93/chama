ab9774bc38a8b2c37a8561c620a78858c1c57e1e
luandev93 <luandev93@gmail.com>
2026-08-27 09:23:31 -0300

    feat(payments): wire controller and provider adapter

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { MercadoPagoProvider } from './providers/mercado-pago.provider';
 
-@Module({ providers: [PaymentsService], exports: [PaymentsService] })
+@Module({
+  controllers: [PaymentsController],
+  providers: [PaymentsService, MercadoPagoProvider],
+  exports: [PaymentsService],
+})
 export class PaymentsModule {}
